796721b1e98947281294d497ed30bd9b07d767b6
soyeun <thduswkd96@gmail.com>
2025-11-03 16:36:52 +0900

    feat(main): 입력 예외 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,9 +10,10 @@ public class Application {
                 System.out.println("구입금액을 입력해 주세요.");
                 String input = Console.readLine();
 
-                int money = inputBlankValidation(input);
+                int money = validateInputBlank(input);
 
-                checkMoneyValidation(money);
+                validateMoneyPositive(money);
+                validateThousandUnit(money);
 
                 break;
             } catch (IllegalArgumentException e) {
@@ -23,16 +24,22 @@ public class Application {
         }
     }
 
-    public static int inputBlankValidation(String input) {
+    public static int validateInputBlank(String input) {
         if (input == null || input.trim().isBlank()) {
             throw new IllegalArgumentException("[ERROR] 값이 입력되지 않았습니다.");
         }
         return Integer.parseInt(input);
     }
 
-    public static void checkMoneyValidation(int money) {
+    public static void validateMoneyPositive(int money) {
         if (money <= 0) {
             throw new IllegalArgumentException("[ERROR] 입력된 금액이 음수입니다.");
+        }
+    }
+
+    public static void validateThousandUnit(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.");
         }
     }
 
